4da84ab0c1e0c4537f6a155fde678f5a5c8f1dee
Zhao Zhongming_CN04083 <ZHAOZ1@airproducts.com>
2016-11-11 20:23:06 +0800

    init

---
+++ ng2Mvc5Demo/gulpfile.js
@@ -0,0 +1,85 @@
+﻿var ts = require('gulp-typescript');
+var gulp = require('gulp');
+var clean = require('gulp-clean');
+var concat = require('gulp-concat');
+var del = require('del');
+var jsMinify = require('gulp-uglify');
+var SystemBuilder = require('systemjs-builder');
+var sourcemaps = require('gulp-sourcemaps');
+var path = require('path');
+var destPath = './dist/';
+
+// Delete the dist directory
+gulp.task('clean', function () {
+    return gulp.src(destPath)
+        .pipe(clean());
+});
+
+gulp.task("scripts:jb", () => {
+    gulp.src([
+            'jquery/dist/jquery.js',
+            'bootstrap/dist/js/bootstrap.js'
+    ], {
+        cwd: "node_modules/**"
+    })
+    .pipe(concat('jb.js'))
+    .pipe(gulp.dest("./dist/js"));
+});
+
+gulp.task("scripts:startup", () => {
+    gulp.src([
+            'core-js/client/shim.min.js',
+            'zone.js/dist/zone.js',
+            'reflect-metadata/reflect.js',
+            'systemjs/dist/system.src.js'
+    ], {
+        cwd: "node_modules/**"
+    })
+    .pipe(concat('startup.js'))
+    .pipe(gulp.dest("./dist/js"));
+});
+
+gulp.task('scripts:bundle', () => {
+    var builder = new SystemBuilder();
+
+    return builder.loadConfig('systemjs.config.js')
+        .then(() => builder.buildStatic('app', 'dist/js/bundle.js'))
+});
+
+var tsProject = ts.createProject('tsconfig.json', {
+    typescript: require('typescript')
+});
+
+gulp.task('scripts:app', function (done) {
+    //var tsResult = tsProject.src()
+    var tsResult = gulp.src([
+            "src/*.ts"
+    ])
+        .pipe(ts(tsProject));
+    return tsResult.js.pipe(gulp.dest('./dist/app'));
+});
+
+gulp.task('watch', ['watch.ts']);
+
+gulp.task('watch.ts', ['ts'], function () {
+    return gulp.watch('src/*.ts', ['ts']);
+});
+
+gulp.task('minify', () => {
+    var js = gulp.src('dist/js/bundle.js')
+        .pipe(jsMinify())
+        .pipe(gulp.dest('dist/js/'));
+});
+
+gulp.task('scripts', ['scripts:jb', 'scripts:startup', "scripts:bundle"], function () {
+    gulp.start('minify');
+});
+
+
+gulp.task('run', ['scripts:app'], function () {
+    gulp.start('scripts');
+});
+
+gulp.task('default', ['clean'], function () {
+    gulp.start('run');
+});
---
+++ ng2Mvc5Demo/systemjs.config.js
@@ -0,0 +1,38 @@
+﻿(function (global) {
+    var map = {
+        'app': 'dist/app',
+        'rxjs': 'node_modules/rxjs',
+        '@angular': 'node_modules/@angular'
+    };
+
+    var packages = {
+        'app': { main: 'main.js', defaultExtension: 'js' },
+        'rxjs': { defaultExtension: 'js' }
+    };
+
+    var angularPackages = [
+        'common',
+        'compiler',
+        'core',
+        'http',
+        'platform-browser',
+        'platform-browser-dynamic',
+        'router'
+    ];
+
+    angularPackages.forEach(function (pkgName) {
+        packages['@angular/' + pkgName] = {
+            main: 'bundles/' + pkgName + '.umd.js',
+            defaultExtension: 'js'
+        };
+    });
+
+    var config = {
+        map: map,
+        packages: packages
+    };
+
+    System.config(config);
+
+})(this);
+
